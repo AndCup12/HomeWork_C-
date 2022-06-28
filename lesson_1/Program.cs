1e4e4f8ca72c6d763d48af86caa330d24f959ba6
Andrey <andrejprasolov93239@gmail.com>
2022-06-28 05:07:29 +0300

    первое задание

--- lesson_1/Program.cs
+++ lesson_1/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿// Задача 2.
+int a = 5;
+int b = 7;
+int max = a;
+
+if(max > b){
+    Console.WriteLine(max);
+}else{
+    max = b;
+    Console.WriteLine(max);
+}
+
+int a1 = 2;
+int b1 = 10;
+int max1 = a1;
+
+if(max1 > b1){
+    Console.WriteLine(max1);
+}else{
+    max1 = b1;
+    Console.WriteLine(max1);
+}
+
+int a2 = -9;
+int b2 = -3;
+int max2 = a2;
+
+if(max2 > b2){
+    Console.WriteLine(max2);
+}else{
+    max2 = b2;
+    Console.WriteLine(max2);
+}
+
+// Задача 4.
+
