574139d457242300a721e5a1b0c4d1e5ffc971e6
Andrey <andrejprasolov93239@gmail.com>
2022-06-28 09:38:46 +0300

    Решения задач

--- lesson_1/Program.cs
+++ lesson_1/Program.cs
@@ -1,36 +1,61 @@
 ﻿// Задача 2.
-int a = 5;
-int b = 7;
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
 int max = a;
 
 if(max > b){
-    Console.WriteLine(max);
+    Console.WriteLine(max + " является максимальным числом");
 }else{
     max = b;
-    Console.WriteLine(max);
+    Console.WriteLine(max + " является максимальным числом");
 }
 
-int a1 = 2;
-int b1 = 10;
-int max1 = a1;
-
-if(max1 > b1){
-    Console.WriteLine(max1);
-}else{
-    max1 = b1;
-    Console.WriteLine(max1);
-}
-
-int a2 = -9;
-int b2 = -3;
-int max2 = a2;
-
-if(max2 > b2){
-    Console.WriteLine(max2);
-}else{
-    max2 = b2;
-    Console.WriteLine(max2);
-}
 
 // Задача 4.
 
+int num_1 = Convert.ToInt32(Console.ReadLine());
+int num_2 = Convert.ToInt32(Console.ReadLine());
+int num_3 = Convert.ToInt32(Console.ReadLine());
+
+int Max = num_1;
+
+if(num_2 > Max){
+    Max = num_2;
+}if(num_3 > Max){
+    Max = num_3;
+}
+
+Console.WriteLine(Max + " является максимальным числом");
+
+// Задача 6.
+
+int firstNum = Convert.ToInt32(Console.ReadLine());
+ if(firstNum % 2 == 0){
+    Console.WriteLine("Число " + " является четным");
+ } else{
+    Console.WriteLine("Число " + " не является четным");
+ }
+
+// Задача8.
+
+int N = Convert.ToInt32(Console.ReadLine());
+int Count = 1;
+
+while(Count < N){
+    if(Count % 2 == 0){
+        Console.Write($"{Count} ");
+    }
+    Count++;
+}
+
+// Задача 7.
+
+int Num = Convert.ToInt32(Console.ReadLine());
+
+if(Num < 100){
+    Console.WriteLine("Введите трехзначное число");    
+}else{
+    int result = Num % 10;
+    Console.WriteLine(result);
+}
