bfed02825404815153b944a5f825e1334906d90f
Andrey <andrejprasolov93239@gmail.com>
2022-07-21 18:14:57 +0300

    ДЗ 5 урок

---
+++ lesson_5/Task_34/Program.cs
@@ -0,0 +1,27 @@
+﻿//  Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+int [] array = new int [10];
+Random rdn = new Random();
+
+void FillPrintArray(int[] arr){
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rdn.Next(100,999);
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+void Counter(int[] arr){
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] % 2 == 0){
+            count++;
+        }
+    }
+    Console.WriteLine($"-> {count}"); 
+}
+Console.Write("[ ");
+FillPrintArray(array);
+Console.Write("]");
+Counter(array);
---
+++ lesson_5/Task_36/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+int[] array = new int[8];
+Random rdn = new Random();
+
+void FillArray(int[] arr){
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rdn.Next(1, 50);
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+void Sum(int[] arr){
+    int sum = 0;
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if(i % 2 == 0 ){
+            sum += arr[i];
+        }
+    }
+    Console.WriteLine(sum);
+}
+
+FillArray(array);
+Console.WriteLine();
+Sum(array);
